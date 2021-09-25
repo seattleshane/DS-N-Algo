622349b55e186f9d6aa106fbb7bc22fd1d9e67f3
seattleshane <shane.a.rideout@gmail.com>
2021-09-25 07:38:59 -0700

    added modern python conventions to show stuff

--- Array.py
+++ Array.py
@@ -1,19 +1,32 @@
-class Array():
-    def __init__(self, length):
-        self.length = length
-        self.values = list()
+from typing import Any
+
+class Array(object):
+    """
+    Defines an array with length. Indexing is donw through magic method
+    __getitem__ to return value at indexed location in the array.  
+    """
+    
+    def __init__(self, length: int):
+        self.length: int = length
+        self.values: list  = []
         for i in range(self.length):
             self.values.append(None)
         
-    def __getitem__(self, index):
-        return self.values[index]
+    def __getitem__(self, index: int) -> Any:
+        if index > len(self.length):
+            return self.values[index]
+        else:
+            raise IndexError
         
     def __setitem__(self, index, value):
-        self.values[index] = value
-        
-        
-a = Array(10)
-print(a.length)
-print(a[2])
-a[2] = 3
-print(a[2])
+        if index > len(self.length):
+            self.values[index] = value
+        else:
+            raise IndexError
+
+if __name__ == "__main__":
+    a = Array(10)
+    print(a.length)
+    print(a[2])
+    a[2] = 3
+    assert a[2] == 3
